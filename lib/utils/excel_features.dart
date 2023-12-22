21649d90edf61ed7a9aa38c2263d5a337afb2730
niwradCodera <63006659+bacardevelopper@users.noreply.github.com>
2023-12-22 21:40:27 +0100

    version stable

--- lib/utils/excel_features.dart
+++ lib/utils/excel_features.dart
@@ -12,22 +12,22 @@ Map<int, List<String>> storeData(Excel excelDecode, String sheetName) {
   Map<int, List<String>> organizedData = {};
 
   for (var rowTable in excelDecode.tables[sheetName]!.rows) {
-    for (var cell in rowTable) {
-      int column = cell!.columnIndex;
-      String value = cell.value.toString();
-      bool containOrNot = organizedData.containsKey(column);
+    for (int columnIndex = 0; columnIndex < rowTable.length; columnIndex++) {
+      var cell = rowTable[columnIndex];
+      String value = cell?.value?.toString() ??
+          ""; // Utilisez une chaîne vide comme valeur par défaut
       bool emptyOrNot = verifyDataInCellEmpty(value);
 
       if (emptyOrNot == false) {
-        if (containOrNot) {
-          organizedData[column]?.add(value);
+        if (organizedData.containsKey(columnIndex)) {
+          organizedData[columnIndex]!.add(value);
         } else {
-          organizedData[column] = [];
-          organizedData[column]?.add(value);
+          organizedData[columnIndex] = [value];
         }
       }
     }
   }
+
   return organizedData;
 }
 
